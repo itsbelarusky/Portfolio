c2522c232524981e2d35fcab4600cd0e9367a39e
itsbelarusky <explosionartx@gmail.com>
2021-09-29 22:43:51 +0300

    create portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,7 +5,41 @@ import "./portfolio.scss"
 const Portfolio = () => {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>layouts</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/slider.jpg" alt=""/>
+                    <h3>Crypto App</h3>
+                </div>
 
+            </div>
         </div>
     );
 };
